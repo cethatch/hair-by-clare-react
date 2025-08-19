51fee5015ce49b993fdba06cc380d9fca2d6483e
cethatch <cethatch@gmail.com>
2025-08-18 21:06:21 -0700

    Started contact page.

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -2,9 +2,35 @@ import React from "react";
 
 function ContactPage() {
     return (
-        <main>
-            On the contact page.
-        </main>
+        <>
+            <div className="contact-top-banner-container">
+                <div className="checkered-border-left checkered-border-half"></div>
+                <h1>Contact Me</h1>
+                <div className="checkered-border-right checkered-border-half"></div>
+                <div className="checkered-border-mobile"></div>
+            </div>
+
+            <div className="form-container">
+                <form>
+                    <h2>Online Inquiry</h2>
+                    <p>Have a question prior to booking an appointment? Submit an inquiry below.</p>
+                    <input type="text" placeholder="Name"></input>
+                    <input type="text" placeholder="Phone Number"></input>
+                    <input type="text" placeholder="Email"></input>
+                    <input type="text" placeholder="Message"></input>
+                </form>
+            </div>
+
+            <div className="details-container">
+                <h2 className="contact-subtitle salon-details-header">Salon Details</h2>
+                <div className="muse-details-container">
+                    <h3 className="muse-header">Muse Salon</h3>
+                    <p className="muse-address salon-address">131 Pringle Ave, Walnut Creek, CA 94596</p>
+                    <p className="muse-hours salon-hours">Hours: Monday - Saturday, 9am - 5pm</p>
+                    <p className="muse-hyperlink salon-hyperlink">www.musesalonwc.com</p>
+                </div>
+            </div>
+        </>
     );
 }
 
